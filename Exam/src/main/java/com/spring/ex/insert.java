a04762df3337f3d02ac3b60b6d4b5e9aa612a722
ramivo08 <hboram8@naver.com>
2022-10-18 12:36:37 +0900

    master 1018 12:36

---
+++ Exam/src/main/java/com/spring/ex/insert.java
@@ -0,0 +1,12 @@
+package com.spring.ex;
+
+public class insert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//insert
+		
+	
+}
---
+++ Exam/src/main/java/com/spring/ex/update.java
@@ -0,0 +1,13 @@
+package com.spring.ex;
+
+public class update {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Update ȣȣ
+		
+	}
+
+}
